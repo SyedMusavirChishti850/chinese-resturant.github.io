d6a2ccc3f18d63d90a8531da5f44dba5235d0864
SyedMusavirChishti850 <82585557+SyedMusavirChishti850@users.noreply.github.com>
2021-07-20 10:42:42 +0500

    Resturant Website

---
+++ js/responseivevoice.js
@@ -0,0 +1,4 @@
+$('a').mouseenter(function() { // Attach this function to all mouseenter events for 'a' tags 
+  responsiveVoice.cancel(); // Cancel anything else that may currently be speaking
+  responsiveVoice.speak($(this).text()); // Speak the text contents of all nodes within the current 'a' tag
+});
